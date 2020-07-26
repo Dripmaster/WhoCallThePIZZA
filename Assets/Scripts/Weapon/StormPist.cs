006587126cc22c486fffdda78dd140f8841aacb4
Soonsung Park <59108907+tnstjd1212@users.noreply.github.com>
2020-07-26 11:15:11 +0900

    Update: StormPist Idle Move Attack
    공격 방향 설정은 했으나 오른쪽이 아닐경우 뒤집힘

--- Assets/Scripts/Weapon/StormPist.cs
+++ Assets/Scripts/Weapon/StormPist.cs
@@ -17,52 +17,26 @@ public class StormPist
 
 public class StormPistIdleStrategy : IdleStrategy
 {
-    SpriteRenderer SR_pist1;
-    SpriteRenderer SR_pist2;
     public void SetState(WeaponBase weaponBase)
     {
-        SR_pist1 = weaponBase.transform.Find("stormpist/pists/pist1").GetComponent<SpriteRenderer>();
-        SR_pist2 = weaponBase.transform.Find("stormpist/pists/pist2").GetComponent<SpriteRenderer>();
         if (weaponBase.CanAttackCancel)
             weaponBase.setState((int)PlayerState.idle);
     }
     public void Update(WeaponBase weaponBase)
     {
-        if (weaponBase.ViewDirection >= 2 && weaponBase.ViewDirection <= 6)  //오른쪽(3, 4, 5)
-        {
-            SR_pist1.sortingOrder = 1;
-            SR_pist2.sortingOrder = -1;
-        }
-        else // 왼쪽 (0, 1, 2, 6, 7)
-        {
-            SR_pist1.sortingOrder = -1;
-            SR_pist2.sortingOrder = 1;
-        }
+        weaponBase.setRotate(weaponBase.ViewDirection * -45f + 180);
     }
 }
 public class StormPistMoveStrategy : MoveStrategy
 {
-    SpriteRenderer SR_pist1;
-    SpriteRenderer SR_pist2;
     public void SetState(WeaponBase weaponBase)
     {
-        SR_pist1 = weaponBase.transform.Find("stormpist/pists/pist1").GetComponent<SpriteRenderer>();
-        SR_pist2 = weaponBase.transform.Find("stormpist/pists/pist2").GetComponent<SpriteRenderer>();
         if (weaponBase.CanAttackCancel)
             weaponBase.setState((int)PlayerState.move);
     }
     public void Update(WeaponBase weaponBase)
     {
-        if (weaponBase.ViewDirection >= 2 && weaponBase.ViewDirection <= 6)  //오른쪽(3, 4, 5)
-        {
-            SR_pist1.sortingOrder = 1;
-            SR_pist2.sortingOrder = -1;
-        }
-        else // 왼쪽 (0, 1, 2, 6, 7)
-        {
-            SR_pist1.sortingOrder = -1;
-            SR_pist2.sortingOrder = 1;
-        }
+        weaponBase.setRotate(weaponBase.ViewDirection * -45f + 180);
     }
 }
 public class StormPistDeadStrategy : DeadStrategy
@@ -112,6 +86,7 @@ public class StormPistAttackStrategy : AttackValues, AttackStrategy
     }
     public void SetState(WeaponBase weaponBase)
     {
+        weaponBase.setRotate(weaponBase.ViewDirection * -45f + 180);
         CountCombo(weaponBase, (int)PlayerState.attack, MoveWhileAttack.Move_Attack);
     }
     public void Update(WeaponBase weaponBase)
